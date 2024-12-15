438dd746ec55d06a0cbce031ce5fc52c797a9570
Muhammad Musabbir <musabbir666@gmail.com>
2024-12-15 17:17:12 +0600

    My first portfolio

---
+++ src/app/data/NavMenu.js
@@ -0,0 +1,27 @@
+export const NavMenu = [
+  {
+    name: 'Muhammad Musabbir',
+    submenu: false,
+    ink: 'Name',
+  },
+  {
+    name: 'Design',
+    submenu: false,
+    link: 'Design',
+  },
+  {
+    name: 'About Me',
+    submenu: false,
+    link: 'about',
+  },
+  {
+    name: 'Resume',
+    submenu: false,
+    link: 'Resume',
+  },
+  {
+    name: 'Contact',
+    submenu: false,
+    link: 'Contact',
+  },
+];
---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Torso from '@/app/bricks/Torso'
+import Segment from '@/app/bricks/Segment';
+import Contact from '@/app/bricks/Contact'
+
+export default function Home() {
+  return (
+    <>
+      <Torso />
+      <Segment />
+      <Contact />
+    </>
+  );
+}
